4063da0bbbdc2b302a02b2078cb9cc2ba20ba934
Anson Tang <32215233+ansontang216@users.noreply.github.com>
2021-04-03 04:00:53 -0400

    added the script commands to delimit directions column and insert into the instructions table

--- normalization_tables.sql
+++ normalization_tables.sql
@@ -18,10 +18,39 @@ FOREIGN KEY(ingredient_id) REFERENCES Ingredients(ingredient_id)
 CREATE TABLE Instructions
 (
 recipe_id int NOT NULL,
-step int NOT NULL AUTO_INCREMENT,
-description,
+step int NOT NULL,
+description text,
 FOREIGN KEY(recipe_id) REFERENCES CleanRecipes(recipe_id)
 );
+
+create temporary table instruction_numbers as (
+
+  select 1 as n
+  union select 2 as n
+  union select 3 as n
+  union select 4 as n
+  union select 5 as n
+  union select 6 as n
+  union select 7 as n
+  union select 8 as n
+  union select 9 as n
+  union select 10 as n
+  union select 11 as n
+  union select 12 as n
+);
+
+insert into Instructions (recipe_id,step,description)
+select recipe_id,
+	n as step,
+ substring_index(
+    substring_index(directions, '**', n), 
+    '**', 
+    -1
+  ) as description
+from CleanRecipes
+join numbers
+  on 
+  ROUND(LENGTH(directions) - LENGTH(REPLACE(directions,"**","")))/ LENGTH("**") > n - 1;
 
 CREATE TABLE Users
 (
